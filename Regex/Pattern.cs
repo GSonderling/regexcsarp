4e1c81485368efd187b2a54f951ef1b56a720200
GSonderling <GSonderling@users.noreply.github.com>
2019-10-04 19:50:10 +0200

    Initial code and project files.

---
+++ Regex/Pattern.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Regex
+{
+    public class Pattern
+    {
+        int position;
+        string uncompiled_expression;
+        string compiled_expression;       
+        
+        int current_rep;
+        int repetition_count;
+        public Pattern(string pattern_string)
+        {
+           
+            repetition_count = 0;
+            position = 0;
+            uncompiled_expression = pattern_string;
+        }
+
+        public int Compile_expression()
+        {
+            for (int index = 0; index < uncompiled_expression.Length; index++)
+            {
+                
+                compiled_expression += uncompiled_expression[index];
+                
+            }
+            return 0;
+        }
+        public int Check_expression(string input_string)
+        {
+            for (int input_index = 0; input_index < input_string.Length; input_index++)
+            {
+                Check_next(input_string[input_index]);
+                if (position == uncompiled_expression.Length)
+                {
+                    position = 0;
+                    return 0;
+                }
+            }
+            position = 0;
+            return 1;
+        }
+        int Check_next(char character)
+        {
+            
+            //Are we done? / matched?
+            if (position+2 > uncompiled_expression.Length)
+            {
+                return 0;
+            }
+            //Skip the char if optional
+            if (uncompiled_expression[position + 1] == '*' &&
+                character != uncompiled_expression[position])
+            {
+                position += 2;
+            }
+            //Special chars/commands incoming
+            if (character == uncompiled_expression[position])
+            {                
+                position++;               
+            }            
+            //Repetition?
+            else if (uncompiled_expression[position] == '*')
+            {      
+                if (character == uncompiled_expression[position + 1])
+                {
+                    position += 2;
+                }
+                else if (character != uncompiled_expression[position-1])
+                {
+                    position = 0;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ RegexTester/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Regex;
+
+namespace RegexTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Positive assertions Pattern
+            */
+            try
+            {
+                Pattern pattern = new Pattern("Hel*o");
+                Debug.Assert(pattern.Check_expression("Hello") == 0);
+                Debug.Assert(pattern.Check_expression("Hello World") == 0);
+                Debug.Assert(pattern.Check_expression("ajaHello World") == 0);
+                Debug.Assert(pattern.Check_expression("akalHello") == 0);
+                Debug.Assert(pattern.Check_expression("abcdHellobcsdWorld") == 0);
+                Debug.Assert(pattern.Check_expression("10Hello5557jjkWorld") == 0);
+
+                pattern = new Pattern("Hellg*o");
+                Debug.Assert(pattern.Check_expression("Hello") == 0);
+                Debug.Assert(pattern.Check_expression("Hello World") == 0);
+                Debug.Assert(pattern.Check_expression("ajaHello World") == 0);
+                Debug.Assert(pattern.Check_expression("akalHello") == 0);
+                Debug.Assert(pattern.Check_expression("abcdHellobcsdWorld") == 0);
+                Debug.Assert(pattern.Check_expression("10Hello5557jjkWorld") == 0);
+
+                Console.WriteLine("Positive Pattern assertions: OK");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Positive Pattern assertions: ERROR");
+                Console.WriteLine(e);
+            }
+
+            /*
+             Negative assertions Pattern
+             */
+            try
+            {
+                Pattern pattern = new Pattern("Hell*o");
+                Debug.Assert(pattern.Check_expression("Heggo") == 1);
+                Debug.Assert(pattern.Check_expression("Heggo World") == 1);
+                Debug.Assert(pattern.Check_expression("HELLO WORLD") == 1);
+                Debug.Assert(pattern.Check_expression("ajaHeo World") == 1);
+                Debug.Assert(pattern.Check_expression("oll*eH") == 1);
+                Debug.Assert(pattern.Check_expression("abcdhellobcsdWorld") == 1);
+                Debug.Assert(pattern.Check_expression("10HELLO5557jjkWorld") == 1);
+
+                Console.WriteLine("Negative Pattern assertions: OK");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Negative Pattern assertions: ERROR");
+                Console.WriteLine(e);
+            }
+
+            /*
+             Positive assertions PatternMatcher
+             */
+            try
+            {
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "ello") == 0);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "e") == 0);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "Wor") == 0);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "ld") == 0);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "Hello") == 0);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "Hello World") == 0);
+
+                Console.WriteLine("Positive PatternMatcher assertions: OK");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Positive PatternMatcher assertions: ERROR");
+                Console.WriteLine(e);
+            }
+
+            /*
+             Negative assertions PatternMatcher
+             */
+            try
+            {
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "eelo") == 1);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "X") == 1);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "x") == 1);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "Heello") == 1);
+                Debug.Assert(PatternMatcher.Match_pattern("Hello World", "Hello Worldd") == 1);
+
+                Console.WriteLine("Negative PatternMatcher assertions: OK");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Negative PatternMatcher assertions: ERROR");
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("...");
+            Console.ReadKey();
+        }
+    }
+}
