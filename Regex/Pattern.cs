4bdfb7d0864de403a5b76ecdb6b440345b28456d
GSonderling <GSonderling@users.noreply.github.com>
2019-10-08 14:26:45 +0200

    Groundwork for actual utility. Removed superfluous dependencie. Text file man.txt, made up from several man pages, will be used for testing of the cl utility in the future.

--- Regex/Pattern.cs
+++ Regex/Pattern.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 namespace Regex
 {
     public class Pattern
---
+++ grapefruit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Regex;
+
+namespace grapefruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern("");
+            CheckFile(args[0],pattern);
+        }
+
+        static int CheckFile(string path, Pattern pattern,int outputMode = 0)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    string line;
+                    //Iterate over file
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //Print lines with matches
+                        if (pattern.Check_expression(line) == 0 && outputMode == 0)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Error: no file in: " + path);
+                return 1;
+            }
+        }
+    }
+}
