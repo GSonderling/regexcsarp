85e6a9661867953228d3d5f5d3b47137cc3fa9ab
GSonderling <GSonderling@users.noreply.github.com>
2019-12-04 15:40:56 +0100

    Finishing touches on some edge cases, implemented new unit tests and added some functionality to autotest.sh so it now prints wc output for both files

--- grapefruit/autotest.sh
+++ grapefruit/autotest.sh
@@ -4,3 +4,9 @@
 grep "[Bb]ytes*" data/man.txt > testgrep;
 bin/debug/grapefruit.exe "B|bytes*" "data/man.txt" > testgrapefruit;
 diff -ywB --width=150 testgrep testgrapefruit;
+
+echo "wc stats for grep output:"
+wc testgrep
+
+echo "wc stats for grapefruit output:"
+wc testgrapefruit
